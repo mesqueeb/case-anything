41edc507080d347923b390d192d449f48e9c8274
Luca Ban <mesqueeb@gmail.com>
2020-08-30 10:01:33 +0900

    chore: update dependencies

--- build/rollup.js
+++ build/rollup.js
@@ -22,9 +22,7 @@ const pkg = require('../package.json')
 const name = pkg.name
 const className = name.replace(/(^\w|-\w)/g, c => c.replace('-', '').toUpperCase())
 const external = Object.keys(pkg.dependencies || [])
-const plugins = [
-  typescript({useTsconfigDeclarationDir: true}),
-]
+const plugins = [typescript({ useTsconfigDeclarationDir: true, tsconfigOverride: { exclude: ['test/**/*'] } })]
 
 // ------------------------------------------------------------------------------------------
 // Builds
@@ -33,14 +31,17 @@ function defaults (config) {
   // defaults
   const defaults = {
     plugins,
-    external
+    external,
   }
   // defaults.output
   config.output = config.output.map(output => {
-    return Object.assign({
-      sourcemap: false,
-      name: className,
-    }, output)
+    return Object.assign(
+      {
+        sourcemap: false,
+        name: className,
+      },
+      output
+    )
   })
   return Object.assign(defaults, config)
 }
@@ -49,8 +50,8 @@ export default [
   defaults({
     input: 'src/index.ts',
     output: [
-      {file: 'dist/index.cjs.js', format: 'cjs'},
-      {file: 'dist/index.esm.js', format: 'esm'},
+      { file: 'dist/index.cjs.js', format: 'cjs' },
+      { file: 'dist/index.esm.js', format: 'esm' },
     ],
   }),
 ]
